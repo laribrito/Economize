c1d45d11c9892779141e21c4aa9e4f8daad7fc96
laribrito <lary.29.ds@gmail.com>
2021-11-01 19:46:55 -0300

    Adiciona os avisos de licença

--- appConfig.py
+++ appConfig.py
@@ -1,4 +1,4 @@
-'''
+"""
 Classe AppConfig.
 
 Esta classe será responsável por manipular algumas configurações da aplicação.
@@ -8,7 +8,24 @@ Altere o atributo 'servidor' desta classe para indicar o endereço do web servic
 
 obs.: Basicamente eu copiei esse arquivo do repositório do meu prof e vou estudar a lógica 
 por trás desse arquivo. Obrigada JP!
-'''
+
+###################################################################################
+
+This file is part of Economize!.
+
+Economize! is free software: you can redistribute
+it and/or modify it under the terms of the GNU General Public License 
+as published by the Free Software Foundation, either version 3 of
+the License, or any later version.
+
+Economize! is distributed in the hope that 
+it will be useful, but WITHOUT ANY WARRANTY; without even the 
+implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+PURPOSE.  See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Economize!.  If not, see <https://www.gnu.org/licenses/>.
+"""
 
 import json
 
