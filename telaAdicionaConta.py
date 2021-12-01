9754cee1e6df2fec873b0e1dea7472e9a8cc362e
laribrito <lary.29.ds@gmail.com>
2021-12-01 09:48:17 -0300

    Adiciona um menu dropdown para o tipo
    O tipo era uma caixa de texto e agora é um menu dropdown que pode
    ser facilmente manipulado. Além de visualmente mais bonito, isso
    retira a possibilidade de erros nesse campo.

--- telaAdicionaConta.py
+++ telaAdicionaConta.py
@@ -18,6 +18,11 @@ along with Economize!.  If not, see <https://www.gnu.org/licenses/>.
 from kivy.uix.screenmanager import Screen
 from kivy.lang import Builder
 from kivy.properties import ObjectProperty
+from kivy.uix.button import Button
+from kivy.metrics import dp
+from kivy.uix.dropdown import DropDown
+from kivy.core.window import Window
+from kivy.clock import Clock
 
 #carrega a tela .kv correspondente
 Builder.load_file("telas/adicionaConta.kv")
@@ -28,15 +33,69 @@ from model import db
 #importa as configurações gerais do sistema
 from appConfig import AppConfig
 
+class BtnPrincipal(Button):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.height= self.texture_size[1] + dp(40)
+        self.markup=True
+        self.background_normal="telas/imgs/bordaBotao.png"
+        self.background_down="telas/imgs/bordaBotao.png"
+        self.color="#272727"
+
+class BtnDropDown(Button):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.background_normal="telas/imgs/borda.png"
+        self.background_color="#FFFFFF"
+        self.color="#000000"
+
 class AdicionaConta(Screen):
 
     #elementos da interface
     setMensagem = ObjectProperty(None)
     getNome = ObjectProperty(None)
     getPadrao = ObjectProperty(None)
-    getTipo = ObjectProperty(None)
+    # getTipo = ObjectProperty(None)
+    layoutTipo = ObjectProperty(None)
 
-    def cadastraConta(self, nome, tipo, padrao):
+    #Tipos de conta que o programa trabalha
+    tiposDisponiveis=["Dinheiro", "Saldo"   , "Investimento"]
+
+    #Tipo selecionado
+    tipoEscolhido = ""
+
+    def __init__(self, **kw):
+        super().__init__(**kw)
+        #Menu dropdown
+        
+        #Botão principal
+        btn_principal = BtnPrincipal(text="Escolha o tipo")
+        self.layoutTipo.add_widget(btn_principal)
+        
+        #Itens dropdown
+        dropdown = DropDown()
+        for index in range(len(self.tiposDisponiveis)):
+            btn = BtnDropDown(text = f'[color=#ffffff00]{index+1}[/color]{self.tiposDisponiveis[index]} ', size_hint_y = None, height = 44, 
+            on_release = lambda btn: self.escolheuTipo(btn),
+            markup=True)
+
+            # binding the button to show the text when selected
+            btn.bind(on_release = lambda btn: dropdown.select(btn.text))
+
+            # then add the button inside the dropdown
+            dropdown.add_widget(btn)
+        
+        btn_principal.bind(on_release = dropdown.open)
+
+        # one last thing, listen for the selection in the
+        # dropdown list and assign the data to the button text.
+        dropdown.bind(on_select = lambda instance, x: setattr(btn_principal, 'text', x))
+
+    def escolheuTipo(self, instance):
+        self.tipoEscolhido=instance.text[17]
+        print(self.tipoEscolhido)
+        
+    def cadastraConta(self, nome, padrao):
         valido = True
 
         #ERRO: já existe uma conta com o nome informado
@@ -47,9 +106,9 @@ class AdicionaConta(Screen):
 
         #ERRO: 'tipo' não é um número
         try:
-            tipo = int(tipo)
+            tipo = int(self.tipoEscolhido)
             #ERRO: valor de 'tipo' não é um valor disponível
-            if tipo > 3 or tipo < 1:
+            if tipo > len(self.tiposDisponiveis) or tipo < 1:
                 self.setMensagem.text = "Tipo de conta inválido. Valor não disponível."
                 valido = False
         except ValueError:
@@ -72,7 +131,7 @@ class AdicionaConta(Screen):
                 AppConfig.set_config("idConta", novaConta[0])
 
             self.getNome.text = ""
-            self.getTipo.text = ""
+            # self.getTipo.text = ""
             self.getPadrao.active = False
 
             #muda para a tela inicial
